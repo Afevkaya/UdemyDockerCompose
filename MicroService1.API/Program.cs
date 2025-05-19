f7bbd81e831d68de55fa61b11b129d7084b9023b
ahmet.evkaya <ahmet.evkaya@hangikredi.com>
2025-05-19 19:29:42 +0300

    Update Dockerfile and .gitignore; add Swagger support to MicroService1.API

--- MicroService1.API/Program.cs
+++ MicroService1.API/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c=>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "MicroService1.API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,6 +17,13 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MicroService1.API v1");
+    c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+});
 
 app.UseAuthorization();
 
